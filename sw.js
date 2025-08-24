b999985e05bdcd5ed1f5721a25c8689f5363793d
octo701028-sudo <octo701028@gmail.com>
2025-08-25 01:10:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'enervi7-v3';
-const PRECACHE = ['./','./index.html','./manifest.json','./yl_blends.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE = 'enervi7-final-v4-1';
+const PRECACHE = ['./','./index.html','./manifest.json','./yl_blends.json','./oracle_whispering.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch', e=>{
