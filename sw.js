98bff2a24f52493d756f68962a1c18bba3863df9
octo701028-sudo <octo701028@gmail.com>
2025-08-24 16:11:16 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// Enervi7 PWA Service Worker (Octalysis Edition)
+const CACHE_NAME = 'enervi7-octa-v1';
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE)).then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req).then(res => {
+        // Runtime cache GET requests
+        if (req.method === 'GET' && res && res.status === 200 && res.type === 'basic') {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(req, resClone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
