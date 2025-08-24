8267ca376c9110f426d6321f70905d30d51d94dd
octo701028-sudo <octo701028@gmail.com>
2025-08-24 22:14:57 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'enervi7-full-v1';
-const PRECACHE = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./yl_blends.json'];
-self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(PRECACHE)).then(self.skipWaiting())); });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))).then(()=>self.clients.claim())); });
+const CACHE = 'enervi7-final-v1';
+const PRECACHE = ['./','./index.html','./manifest.json','./yl_blends.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting())); });
+self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch', e=>{
   e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(res=>{
-    if(e.request.method==='GET' && res && res.status===200 && res.type==='basic'){ const clone=res.clone(); caches.open(CACHE_NAME).then(ca=>ca.put(e.request,clone)); }
+    if(e.request.method==='GET' && res.status===200 && res.type==='basic'){ const clone=res.clone(); caches.open(CACHE).then(cache=>cache.put(e.request, clone)); }
     return res;
   }).catch(()=>caches.match('./index.html'))));
 });
