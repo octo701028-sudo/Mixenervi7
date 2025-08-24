b60988b296aab119ab6adddcd90687f51715f81f
octo701028-sudo <octo701028@gmail.com>
2025-08-25 01:27:02 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-const CACHE = 'enervi7-final-v4-1';
-const PRECACHE = ['./','./index.html','./manifest.json','./yl_blends.json','./oracle_whispering.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(res=>{
-    if(e.request.method==='GET' && res.status===200 && res.type==='basic'){ const clone=res.clone(); caches.open(CACHE).then(cache=>cache.put(e.request, clone)); }
-    return res;
-  }).catch(()=>caches.match('./index.html'))));
-});
+const CACHE='enervi7-final-v4-2';
+const PRECACHE=['./','./index.html','./manifest.json','./yl_blends.json','./oracle_whispering.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))).then(()=>self.clients.claim())});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(res=>{if(e.request.method==='GET'&&res.status===200&&res.type==='basic'){const clone=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,clone));}return res;}).catch(()=>caches.match('./index.html'))))});
